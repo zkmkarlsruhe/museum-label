9b495560dec46f7987c17e58730c205b4de8bb58
Dan Wilcox <dan.wilcox@zkm.de>
2021-07-02 13:23:48 +0200

    added demo0 and demo1

---
+++ demo1/js/script.js
@@ -0,0 +1,34 @@
+// ref: https://github.com/colinbdclark/osc.js/
+
+let greeting = ["...", "Hello", "Bonjour", "Guten Tag", "Hola"]
+
+// use localhost when only connecting internally
+let host = "localhost"
+
+// use the server machine's IP address when connecting external devices
+//let host = "10.10.0.159"
+
+// OSC
+
+var oscPort = new osc.WebSocketPort({
+    url: "ws://"+host+":8081",
+    metadata: true
+})
+
+oscPort.on("message", function (message) {
+    console.log("received osc: ", message)
+   	if(message.address == "/detecting") {
+   		if(message.args[0].value == 0) {
+   			document.getElementById("detecting").style.display = "none"
+   		}
+   		else {
+   			document.getElementById("detecting").style.display = "inline"
+   		}
+   	}
+   	else if(message.address == "/lang") {
+   		document.getElementById("greeting").innerHTML = greeting[message.args[0].value]
+        document.getElementById("flag").innerHTML = flag[message.args[0].value]
+   	}
+})
+
+oscPort.open()
