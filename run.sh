1a0734158a925e53d72ebe93fb1189b179d39b89
Dan Wilcox <dan.wilcox@zkm.de>
2021-09-21 15:02:48 +0200

    added commandline args to run script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 #
 # run to start basic localhost system for testing,
-# quit LanguageIdentifier app to shutdwn everything
+# quit LanguageIdentifier app to shutdown everything
 #
 # Copyright (c) 2021 ZKM | Hertz-Lab
 # Dan Wilcox <dan.wilcox@zkm.de>
@@ -37,6 +37,23 @@ esac
 
 ##### functions
 
+# check argument and exit with error if not set
+# $1 argument name in error print
+# $2 argument
+function checkarg() {
+	local arg=$2
+	local failed=false
+	if [ "$arg" = "" ] ; then
+		failed=true
+	elif [ "${arg%${arg-?}}"x = '-x' ] ; then
+		failed=true
+	fi
+	if [ "$failed" = true ] ; then
+		echo "$1 option requires an argument"
+		exit 1
+	fi
+}
+
 # get the pid by script name, ie. getpidof controller.py -> 16243
 # $1 script or process name
 function getpid() {
@@ -48,6 +65,64 @@ function getpid() {
 		echo $(pidof "$1")
 	fi
 }
+
+##### parse command line arguments
+
+HELP="USAGE: run.sh [OPTIONS]
+
+  run to start basic localhost system for testing,
+  quit LanguageIdentifier app to shutdown everything
+
+Options:
+  -h,--help              display this help message
+  -c,--confidence FLOAT  langid min confidence 0 - 1, default 0.75
+  -t,--threshold INT     langid volume threshold 0 - 100, default 25
+  -l,--list              list audio input devices and exit
+  --inputdev INT         audio input device number
+  --inputchan INT        audio input device channel, default 1
+"
+
+while [ "$1" != "" ] ; do
+	case $1 in
+		-h|--help)
+			echo "$HELP"
+			exit 0
+			;;
+		-c|--confidence)
+			shift 1
+			checkarg "-c,--confidence" $1
+			CONFIDENCE=$1
+			;;
+		-t|--threshold)
+			shift 1
+			checkarg "-t,--threshold" $1
+			THRESHOLD=$1
+			;;
+		-l|--list)
+			$LID -l
+			exit 0
+			;;
+		--inputdev)
+			shift 1
+			checkarg "--inputdev" $1
+			INPUTDEV=$1
+			;;
+		--inputchan)
+			shift 1
+			checkarg "--inputchan" $1
+			INPUTCHAN=$1
+			;;
+		*)
+			break
+			;;
+	esac
+	shift 1
+done
+
+echo "confidence: $CONFIDENCE"
+echo "threshold:  $THRESHOLD"
+echo "inputdev:   $INPUTDEV"
+echo "inputchan:  $INPUTCHAN"
 
 ##### main
 
