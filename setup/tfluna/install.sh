cd7a5fc7a85ecff50337c0966b051588ddacfd8d
bethge <bethge@zkm.de>
2021-09-27 16:15:27 +0200

    enhance install script

--- setup/tfluna/install.sh
+++ setup/tfluna/install.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 path=$(dirname $(realpath $0))
+apppath=$path/mini.py
 cd $path 
 homedir=$(eval echo ~$(whoami))
 user=$(whoami)
 
-sudo systemctl mask serial-getty@ttyAMA0.service # https://raspberrypi.stackexchange.com/a/48258 
-
+# https://raspberrypi.stackexchange.com/a/48258 
+sudo systemctl mask serial-getty@ttyAMA0.service 
 sudo adduser $user dialout
+
+# install requirements
 sudo apt update && sudo apt upgrade -y
 sudo apt install python-pip -y
 pip install -r requirements.txt
 
-sudo rm /etc/systemd/system/tfmini.service
-sudo systemctl daemon-reload
-cp tfmini.service.default tfmini.service
-sed -i s/pi/$user/g tfmini.service
+# remove any existing tfmini service and restart daemon
+./remove_service.sh
 
+# fill in the service template with user and path
+cp tfmini.service.default tfmini.service
+sed -i s?'USER'?$user? tfmini.service
+sed -i s?'PATH'?$apppath? tfmini.service
+
+# create a symbolic link and start the service
 sudo ln -s $path/tfmini.service /etc/systemd/system/
 sudo systemctl enable tfmini
 sudo systemctl start tfmini
+sudo service tfmini status
---
+++ setup/tfluna/remove_service.sh
@@ -0,0 +1,11 @@
+SNAME=tfmini
+
+rm $SNAME.service
+
+sudo systemctl stop $SNAME
+sudo systemctl disable $SNAME
+
+sudo rm /etc/systemd/system/$SNAME
+
+sudo systemctl daemon-reload
+sudo systemctl reset-failed
