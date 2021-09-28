0cfc5af02ec93f8dcdd84619d77b40db7213cd7a
bethge <bethge@zkm.de>
2021-09-28 17:34:07 +0200

    upgrade to python3 for osc

--- setup/tfluna/mini.py
+++ setup/tfluna/mini.py
@@ -24,33 +24,49 @@ ser = serial.Serial("/dev/ttyAMA0", 115200)
 
 def getTFminiData(client, args):
 
+    last_distance = -1
+    max_distance = args.max_distance
+
     while True:
         
         count = ser.in_waiting
         
         if count > 4:
-        
             recv = ser.read(4)
             ser.reset_input_buffer()
             
-            #print(recv)
+            # print(str(recv[0]) + " " +  str(recv[1]) + " " +str(recv[2]) + " " +str(recv[3]) )
 
-            if recv[0] == 'Y' and recv[1] == 'Y' and not recv[2] == 'Y': # 0x59 is 'Y'
+            # check if input is valid
+            if recv[0] == 89 and recv[1] == 89: 
    
-                low = int(recv[2].encode('hex'), 16)
-                high = int(recv[3].encode('hex'), 16)
+                # interpret upper two bytes as one 16bit int
+                low = int(recv[2])
+                high = int(recv[3])
                 distance = low + high * 256
 
-                max_distance = 800
+                # cap the distance
                 if distance > max_distance:
                     distance = max_distance
+                if distance < 0:
+                    distance = 0
                 
-                distance = translate(distance, 0, max_distance, 0, 1)
+                # map the range to 0.0 (far away) and 1.0 (closest)
+                distance = translate(distance, 0, max_distance, 1, 0)
 
-                if args.use_osc:
-                    client.send_message("/listen", distance)
-                else:
-                    client.sendto(distance, (args.destination, args.port))
+                # check if difference is large enough
+                if abs(distance - last_distance) >= args.epsilon:
+
+                    #print(distance)
+
+                    # choose a protocol
+                    if args.use_udp:
+                        message = str(distance).encode()
+                        client.sendto(message, (args.destination, args.port))
+                    else:
+                        client.send_message("/proximity", distance)
+
+                last_distance = distance
 
         time.sleep(args.interval)
 
@@ -65,30 +81,24 @@ def getTFminiData(client, args):
 # -----------------------------------------------------------------------------------
 if __name__ == '__main__':
 
-
     parser = argparse.ArgumentParser(description='TF Luna Lidar sensor')
-    parser.add_argument('-i', '--interval', type=float, default=0.1, dest='interval', help='interval in seconds (default: 0.1 sec)', metavar='INTERVAL')
+    parser.add_argument('-u', '--use_udp', action='store_true',default=False, dest='use_udp', help='whether to use osc or udp')
     parser.add_argument('-d', '--destination', default='127.0.0.1', dest='destination', help='destination hostname or IP', metavar='HOST')
-    parser.add_argument('-p', '--port', type=int, default='12345', dest='port', help='destination port to send to', metavar='PORT')
-    parser.add_argument('-u', '--use_osc', type=bool, default=True, dest='use_osc', help='', metavar='USE_OSC')
+    parser.add_argument('-p', '--port', type=int, default='54322', dest='port', help='destination port to send to', metavar='PORT')
+    parser.add_argument('-i', '--interval', type=float, default=0.1, dest='interval', help='interval in seconds (default: 0.1 sec)', metavar='INTERVAL')
+    parser.add_argument('-e', '--epsilon', type=float, default=0.05, dest='epsilon', help='minimum difference for sending a package', metavar='EPSILON')
+    parser.add_argument('-m', '--max_distance', type=int, default=800, dest='max_distance', help='maximum allowed distance (rest is capped)', metavar='MAX_DISTANCE')
     args = parser.parse_args()
     print(vars(args))
 
-    if args.use_osc:
-        from pythonosc import udp_client
-        client = udp_client.SimpleUDPClient(args.ip, args.port)
-        
-    else:
+    if args.use_udp:
         import socket
-        # init UDP socket 
         client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
         client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    else:
+        from pythonosc import udp_client
+        client = udp_client.SimpleUDPClient(args.destination, args.port)
 
-
-
-    for x in range(10):
-        time.sleep(1)
-    
     try:
         if ser.is_open == False:
             print("try to open serial port")
