b757adf02fa62d73da12f9215d8234a7e59f6b06
Dan Wilcox <dan.wilcox@zkm.de>
2021-11-30 14:48:36 +0100

    renamed receive test to udpreceive

--- rpi/tfluna/tests/udpreceieve.py
+++ rpi/tfluna/tests/udpreceieve.py
@@ -1,10 +1,11 @@
+#! /usr/bin/env python3
+
 import socket
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 54322
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 sock.bind((UDP_IP, UDP_PORT))
